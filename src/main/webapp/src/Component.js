332800c870782878d4a95f898c51352da89f44c3
tisoferi <tiso.feri@gmail.com>
2015-06-03 14:37:26 +0200

    Step 27: Mock Server & Automatic Model Instantiation

--- src/main/webapp/src/Component.js
+++ src/main/webapp/src/Component.js
@@ -1,9 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/walkthrough/controller/HelloDialog",
-	"sap/ui/model/odata/v2/ODataModel"
-], function (UIComponent, JSONModel, HelloDialog, ODataModel) {
+	"sap/ui/walkthrough/controller/HelloDialog"
+
+], function (UIComponent, JSONModel, HelloDialog) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.walkthrough.Component", {
@@ -24,11 +24,6 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
-
-			// set invoice model - local
-			var oConfig = this.getMetadata().getConfig();
-			var oInvoiceModel = new ODataModel(oConfig.invoiceRemote);
-			this.setModel(oInvoiceModel, "invoice");
 
 			// set dialog
 			this.helloDialog = new HelloDialog();
---
+++ src/main/webapp/test/service/server.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+	], function (MockServer) {
+		"use strict";
+
+		return {
+
+			init: function () {
+				// create
+				var oMockServer = new MockServer({
+					rootUri: "/testDataPath/"
+				});
+				// configure
+				MockServer.config({
+					autoRespond: true,
+					autoRespondAfter: 1000
+				});
+
+				// simulate
+				var sPath = jQuery.sap.getModulePath("sap.ui.walkthrough.test.service");
+				oMockServer.simulate(sPath + "/metadata.xml", sPath);
+				// start
+				oMockServer.start();
+				}
+		};
+});
