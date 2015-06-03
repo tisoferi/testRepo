c7b61ee4c1778f72cf17b65262a98d0e9c0d8c12
tisoferi <tiso.feri@gmail.com>
2015-06-03 15:51:59 +0200

    Step 29: Writing an integration test with OPA

---
+++ src/main/webapp/test/integration/navigationJourney.js
@@ -0,0 +1,19 @@
+sap.ui.require([
+	"sap/ui/test/opaQunit"
+	], function () {
+
+		"use strict";
+
+		QUnit.module("Navigation");
+		opaTest("Should open the hello dialog", function (Given, When, Then) {
+
+			// Arrangements
+			Given.iStartMyAppInAFrame(jQuery.sap.getResourcePath("sap/ui/walkthrough/app/test", ".html"));
+
+			//Actions
+			When.onTheAppPage.iPressTheSayHelloWithDialogButton();
+
+			// Assertions
+			Then.onTheAppPage.iShouldSeeTheHelloDialog().and.iTeardownMyAppFrame();
+		});
+});
